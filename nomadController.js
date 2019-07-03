b598cf2e3f5c69a7b2aff7a8768cae1622ab7f7f
Alexis Creuzot <alexis@monoqle.fr>
2019-07-03 10:31:45 +0200

    expand multi-day reservations

--- nomadController.js
+++ nomadController.js
@@ -10,9 +10,24 @@ String.prototype.capitalize = function() {
 
 module.exports = function () {
 
-    function getDateOfItem(item) {
+    function getNumberOfDaysOfItem(firstDay, lastDay, item) {
+        var start = getStartDateOfItem(item);
+        start = start > firstDay ? start : firstDay;
+        var end = getEndDateOfItem(item);
+        end = end < lastDay ? end : lastDay;
+        let diff =  Math.floor(( Date.parse(start) - Date.parse(end) ) / 86400000);
+        return Math.abs(diff);
+    }
+
+    function getStartDateOfItem(item) {
         var start =  item.start || item.originalStartTime;
         var date =  start.date || start.dateTime;
+        return new Date(date);
+    }
+
+    function getEndDateOfItem(item) {
+        var end =  item.end;
+        var date =  end.date || end.dateTime;
         return new Date(date);
     }
 
@@ -22,7 +37,7 @@ module.exports = function () {
 
         var nomadsArray = new Array();
         var firstDay = new Date(month.getFullYear(), month.getMonth(), 1);
-        var lastDay = new Date(month.getFullYear(), month.getMonth() + 1, 0);
+        var lastDay = new Date(month.getFullYear(), month.getMonth() + 1, 1);
         var timeMin = encodeURIComponent(firstDay.toISOString())
         var timeMax = encodeURIComponent(lastDay.toISOString())
 
@@ -37,8 +52,8 @@ module.exports = function () {
             var data = JSON.parse(body);
 
             var sortedItems = data.items.sort(function(a, b) {
-                var eventA = getDateOfItem(a);
-                var eventB = getDateOfItem(b);
+                var eventA = getStartDateOfItem(a);
+                var eventB = getStartDateOfItem(b);
                 return eventB.getDate() - eventA.getDate();
             })
 
@@ -49,26 +64,31 @@ module.exports = function () {
                 // Only consider items with 'confirmed' status
                 if (item.status != 'confirmed') continue;
 
-                var reservation = getDateOfItem(item);
+                // Allow reservations spanning multiple days
+                var nbDays = getNumberOfDaysOfItem(firstDay, lastDay, item);
 
-                if (reservation < firstDay) continue;
+                // Reverse addition of multiple-day reservations
+                for (var i=nbDays-1; i>=0; i--) {
+                    var reservation = getStartDateOfItem(item);
+                    reservation.setDate(reservation.getDate() + i);
 
-                // Populate nomadsDict
-                // Remove mention of chosen desk via split
-                var name = item.summary.split(' (')[0];
-                var found = nomadsArray.find(function(nomad) {
-                    if(nomad.name == name) {
-                         nomad.reservations.push(reservation);
-                        return true;
+                    // Populate nomadsDict
+                    // Remove mention of chosen desk via split
+                    var name = item.summary.split(' (')[0];
+                    var found = nomadsArray.find(function(nomad) {
+                        if(nomad.name == name) {
+                             nomad.reservations.push(reservation);
+                            return true;
+                        }
+                        return false;
+                    });
+
+                    if (!found){
+                        var nomad = {"name" : name, "reservations": [reservation]};
+                        nomadsArray.push(nomad);
                     }
-                    return false;
-                });
-
-                if (!found){
-                    var nomad = {"name" : name, "reservations": [reservation]};
-                    nomadsArray.push(nomad);
+                    count ++;
                 }
-                count ++;
             }
 
             // Formating
