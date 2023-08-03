52bdc32548c3f2bc9b6730ef19a4f848218ce4c4
Alexis Creuzot <alexis.creuzot@gmail.com>
2023-08-03 10:16:32 -0600

    Add reservation feat

--- app.js
+++ app.js
@@ -3,61 +3,137 @@
 /**
  * Module dependencies.
  */
+const express = require('express'); // Add this line
+const bodyParser = require('body-parser');
+
+
 const app = require('./config');
 const PORT = process.env.PORT || 3000;
+
+const { OAuth2Client } = require('google-auth-library');
+const CLIENT_ID = '204812694051-91bud7uc0p6a450g7tllqj1hnk9iru0q.apps.googleusercontent.com';
+const CLIENT_SECRET = 'loIU7lsNAsxRP6-ABcEYK0GH';
+const REDIRECT_URI = 'http://localhost:3000/auth/callback';
+const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
 app.locals.moment = require('moment');
 const dotenv = require('dotenv').config();
 
 function render404(req, res) {
-      res.status(404);
-      res.render('404');
+    res.status(404);
+    res.render('404');
 }
 
 app.locals.moment = require('moment');
-
+app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     process.stdout.write(`Point your browser to: http://localhost:${PORT}\n`);
 });
-
-/*
- * -------------- Routes --------------
- */
 
 const MAX_MONTH = 12;
 var nomadController = require('./nomadController');
 var api = new nomadController();
 
+/*
+ * -------------- Routes --------------
+ */
+
 // Route for the homepage
-app.route('/').get(function(req, res) {
+app.route('/').get(function (req, res) {
     var today = new Date();
-    var monthValue =  today.getMonth()+"-"+today.getFullYear()
+    var monthValue = today.getMonth() + "-" + today.getFullYear();
     res.redirect('/month/' + monthValue);
 });
 
-// Route that catches any other url and renders the 404 page
-app.route('/month/:month').get(function(req, res) {
+app.post('/add-reservation', (req, res) => {
+    const name = req.body.name;
+    const date = req.body.date;
 
-    var today = new Date();
+    console.log('Received name:', name);
+    console.log('Received date:', date);
 
-    var dateArr = req.params.month.split("-"); 
-    var selectedDate = new Date(dateArr[1], dateArr[0], 1);
-
-    var months = [];
-    for (var i=-1; i<MAX_MONTH; i++) {
-        let month = new Date(today.getFullYear(), today.getMonth() - i, 1);
-        var selected = (selectedDate.getFullYear() == month.getFullYear()) && (selectedDate.getMonth() == month.getMonth())
-        months.push({"month" : month, "selected" : selected});
+    // Check if the user is authenticated
+    if (!oAuth2Client.credentials || !oAuth2Client.credentials.access_token) {
+        // User is not authenticated, redirect to the authentication page
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: ['https://www.googleapis.com/auth/calendar.events'],
+            state: JSON.stringify({ name, date }), // Pass reservation data as state
+        });
+        return res.redirect(authUrl);
     }
 
-    api.getReservations(selectedDate, function(data) {
-        res.render('home', {"data" : data, "months" : months});
+    api.addReservation(oAuth2Client.credentials, name, date, (error) => {
+        if (error) {
+            res.status(500).send('Error adding reservation.');
+        } else {
+            res.redirect('/'); // Redirect to the homepage after adding reservation
+        }
     });
-    
 });
 
 // Route that catches any other url and renders the 404 page
-app.route('/:url').get(function(req, res) {
+app.route('/month/:month').get(function (req, res) {
+    var today = new Date();
+    var dateArr = req.params.month.split("-");
+    var selectedDate = new Date(dateArr[1], dateArr[0], 1);
+
+    var months = [];
+    for (var i = -1; i < MAX_MONTH; i++) {
+        let month = new Date(today.getFullYear(), today.getMonth() - i, 1);
+        var selected = (selectedDate.getFullYear() == month.getFullYear()) && (selectedDate.getMonth() == month.getMonth());
+        months.push({ "month": month, "selected": selected });
+    }
+
+    api.getReservations(selectedDate, function (data) {
+        res.render('home', { "data": data, "months": months });
+    });
+
+});
+
+// Route that catches any other url and renders the 404 page
+app.route('/:url').get(function (req, res) {
     render404(req, res);
+});
+
+// Auth
+
+app.get('/auth', (req, res) => {
+    // Construct the authorization URL with required parameters
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline', // Access type set to offline to obtain refresh token
+        scope: ['https://www.googleapis.com/auth/calendar.events'], // Request access to calendar events
+        state: JSON.stringify({ name, date }),
+    });
+
+    // Redirect the user to the authorization URL
+    res.redirect(authUrl);
+});
+
+app.get('/auth/callback', async (req, res) => {
+    const code = req.query.code;
+
+    try {
+        // Exchange the authorization code for an access token
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+
+        // Retrieve the reservation data from the state parameter
+        const state = JSON.parse(req.query.state);
+        const name = state.name;
+        const date = state.date;
+
+        // Use the access token to add the reservation
+        api.addReservation(tokens, name, date, (error) => {
+            if (error) {
+                res.status(500).send('Error adding reservation.');
+            } else {
+                res.redirect('/'); // Redirect to the homepage after adding reservation
+            }
+        });
+    } catch (error) {
+        console.error('Error retrieving access token:', error);
+        res.status(500).send('Error retrieving access token');
+    }
 });
 
--- nomadController.js
+++ nomadController.js
@@ -1,133 +1,212 @@
-const request = require("request");
-const moment = require('moment-timezone');
+const { google } = require("googleapis");
+const { OAuth2Client } = require('google-auth-library');
+const moment = require("moment-timezone");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 const cal_id = process.env.CALENDAR_ID;
 const api_key = process.env.API_KEY;
 
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1)
-}
+const CLIENT_ID = '204812694051-91bud7uc0p6a450g7tllqj1hnk9iru0q.apps.googleusercontent.com';
+const CLIENT_SECRET = 'loIU7lsNAsxRP6-ABcEYK0GH';
+const REDIRECT_URI = 'http://localhost:3000/auth/callback';
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Initialize Google Calendar API with the API key directly
+const calendar = google.calendar({
+  version: "v3",
+  auth: api_key,
+});
 
 module.exports = function () {
+    
+    const calendar = google.calendar({
+        version: "v3",
+        auth: api_key, // Use the API key directly
+    });
 
     function getNumberOfDaysOfItem(firstDay, lastDay, item) {
         var start = getStartDateOfItem(item);
         start = start > firstDay ? start : firstDay;
         var end = getEndDateOfItem(item);
         end = end < lastDay ? end : lastDay;
-        let diff =  Math.floor(( Date.parse(start) - Date.parse(end) ) / 86400000);
+        let diff = Math.floor((Date.parse(start) - Date.parse(end)) / 86400000);
         return Math.abs(diff);
     }
 
     function getStartDateOfItem(item) {
-        var start =  item.start || item.originalStartTime;
-        var date =  start.date || start.dateTime;
+        var start = item.start || item.originalStartTime;
+        var date = start.date || start.dateTime;
         var start = new Date(date);
         return moment(start).utc();
     }
 
     function getEndDateOfItem(item) {
-        var end =  item.end;
-        var date =  end.date || end.dateTime;
+        var end = item.end;
+        var date = end.date || end.dateTime;
         var end = new Date(date);
         return moment(end).utc();
     }
 
+    this.addReservation = function (credentials, name, date, callback) {
 
-    this.getReservations = function(month, callback){        
+        const startDateTime = moment(date).startOf('day');
+        const endDateTime = moment(date).startOf('day').add(1, 'days');
+        const startFormatted = startDateTime.format('YYYY-MM-DD');
+        const endFormatted = endDateTime.format('YYYY-MM-DD');
+        
+        const authClient = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+        authClient.setCredentials(credentials);
 
-        moment.locale('fr');
+        const event = {
+            'summary': name,
+            'location': "Happy Hours, 2 rue Victor Hugo, 35000 RENNES",
+            'description': "Réservation de " + name,
+            'start': {
+                'date': startFormatted,
+                'timeZone': 'Europe/Paris', // Paris timezone
+            },
+            'end': {
+                'date': endFormatted,
+                'timeZone': 'Europe/Paris', // Paris timezone
+            },
+            'transparency': 'transparent',
+        };
 
-        var nomadsArray = new Array();
-        var firstDay = new Date(month.getFullYear(), month.getMonth(), 1);
-        var lastDay = new Date(month.getFullYear(), month.getMonth() + 1, 1);
-        var timeMin = encodeURIComponent(moment(firstDay).utc().toISOString())
-        var timeMax = encodeURIComponent(moment(lastDay).utc().toISOString())
-
-        console.log(firstDay)
-        console.log(lastDay)
-        console.log(timeMin)
-        console.log(timeMax)
-
-
-        const url = "https://www.googleapis.com/calendar/v3/calendars/" + cal_id 
-        + "/events?orderBy=updated&key=" + api_key
-        + "&showDeleted=" + false
-        + "&singleEvents=" + true
-        + "&timeMin="+timeMin
-        + "&timeMax="+timeMax;
-
-        request.get(url, (error, response, body) => {
-            var data = JSON.parse(body);
-
-            if (data["error"]) {
-                callback({ "error" : data["error"]["message"] });
-                return
+        console.log(event)
+        
+        calendar.events.insert(
+            {
+            version: "v3",
+            auth: authClient,
+            calendarId: cal_id,
+            resource: event,
+            },
+            function (err, event) {
+            if (err) {
+                console.log("There was an error contacting the Calendar service: " + err);
+                callback(err);
+                return;
             }
-
-            var sortedItems = data.items.sort(function(a, b) {
-                var eventA = getStartDateOfItem(a);
-                var eventB = getStartDateOfItem(b);
-                return eventB - eventA;
-            })
-
-            console.log(sortedItems)
-
-            var count = 0;
-            for (index in sortedItems) {
-                let item = data.items[index];
-
-                // Only consider items with 'confirmed' status
-                if (item.status != 'confirmed' && item.status != 'tentative') continue;
-
-                // Allow reservations spanning multiple days
-                var nbDays = getNumberOfDaysOfItem(firstDay, lastDay, item);
-
-                // Reverse addition of multiple-day reservations
-                for (var i=nbDays-1; i>=0; i--) {
-                    var reservation = getStartDateOfItem(item);
-                    reservation.add(i, 'd')
-                    // Handle undefined summary
-                    if (item.summary == undefined) {
-                        break;
-                    }
-
-                    // Populate nomadsDict
-                    // Remove mention of chosen desk via split
-                    var name = item.summary.split(' (')[0];
-                    var found = nomadsArray.find(function(nomad) {
-                        if(nomad.name == name) {
-                           nomad.reservations.push(reservation);
-                           return true;
-                       }
-                       return false;
-                   });
-
-                    if (!found){
-                        var nomad = {"name" : name, "reservations": [reservation]};
-                        nomadsArray.push(nomad);
-                    }
-                    count ++;
-                }
+            console.log("Event created: %s", event.data.htmlLink);
+            callback(null);
             }
+        );
+    };
+      
 
-            // Formating
-            var finalArray = [];
-            nomadsArray.forEach( (nomad) => {
-                var dates = nomad.reservations.reverse().map(function(date) {
-                    return moment(date).utc().format("dddd Do").capitalize();
-                });
-                finalArray.push({"name" : nomad.name, "reservations" : dates.join(", "), "count" : nomad.reservations.length})
-            });
+  this.getReservations = function (month, callback) {
+    moment.locale("fr");
 
-            finalArray = finalArray.sort(function(a, b) {
-                return b.count - a.count;
-            });
+    const today = new Date();
+    const nextDay = new Date(today);
+    nextDay.setDate(nextDay.getDate() + 1);
+    const nextDayFormatted = `${nextDay.getFullYear()}-${(nextDay.getMonth() + 1).toString().padStart(2, '0')}-${nextDay.getDate().toString().padStart(2, '0')}`;
 
-            var finalData = {"nbDays" : count,
-            "nomads": finalArray}
+    var nomadsArray = [];
+    var firstDay = new Date(month.getFullYear(), month.getMonth(), 1);
+    var lastDay = new Date(month.getFullYear(), month.getMonth() + 1, 1);
+    var timeMin = moment(firstDay).utc().toISOString();
+    var timeMax = moment(lastDay).utc().toISOString();
 
-            callback(finalData);
+    console.log(firstDay);
+    console.log(lastDay);
+    console.log(timeMin);
+    console.log(timeMax);
+
+    calendar.events.list(
+      {
+        calendarId: cal_id,
+        orderBy: "updated",
+        showDeleted: false,
+        singleEvents: true,
+        timeMin: timeMin,
+        timeMax: timeMax,
+      },
+      (err, response) => {
+        if (err) {
+          callback({ error: err.message });
+          return;
+        }
+
+        var sortedItems = response.data.items.sort(function (a, b) {
+          var eventA = getStartDateOfItem(a);
+          var eventB = getStartDateOfItem(b);
+          return eventB - eventA;
         });
-    }
-}
+
+        console.log(sortedItems);
+
+        var count = 0;
+        for (let index in sortedItems) {
+          let item = sortedItems[index];
+
+          // Only consider items with 'confirmed' status
+          if (item.status !== "confirmed" && item.status !== "tentative") continue;
+
+          // Allow reservations spanning multiple days
+          var nbDays = getNumberOfDaysOfItem(firstDay, lastDay, item);
+
+          // Reverse addition of multiple-day reservations
+          for (let i = nbDays - 1; i >= 0; i--) {
+            var reservation = getStartDateOfItem(item);
+            reservation.add(i, "d");
+            // Handle undefined summary
+            if (item.summary == undefined) {
+              break;
+            }
+
+            // Populate nomadsDict
+            // Remove mention of chosen desk via split
+            var name = item.summary.split(" (")[0];
+            var found = nomadsArray.find(function (nomad) {
+              if (nomad.name == name) {
+                nomad.reservations.push(reservation);
+                return true;
+              }
+              return false;
+            });
+
+            if (!found) {
+              var nomad = { name: name, reservations: [reservation] };
+              nomadsArray.push(nomad);
+            }
+            count++;
+          }
+        }
+
+        // Formating
+        var finalArray = [];
+        nomadsArray.forEach((nomad) => {
+          var dates = nomad.reservations
+            .reverse()
+            .map(function (date) {
+              return moment(date).utc().format("dddd Do").capitalize();
+            });
+          finalArray.push({
+            name: nomad.name,
+            reservations: dates.join(", "),
+            count: nomad.reservations.length,
+          });
+        });
+
+        finalArray = finalArray.sort(function (a, b) {
+          return b.count - a.count;
+        });
+
+        var finalData = {nextDayFormatted:nextDayFormatted, nbDays: count, nomads: finalArray };
+
+        callback(finalData);
+      }
+    );
+  };
+};
+
